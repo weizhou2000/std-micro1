938fb075dbadb1541d03d10cb6c299d6ce41d2f4
think@163.com <think@163.com>
2018-01-20 20:59:24 +0800

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = () => 'Hello world'
+//module.exports = () => 'Hello world'
+
+const { send } = require('micro')
+const { router, get } = require('microrouter')
+
+const hello = (req, res) =>
+  send(res, 200, `Hello ${req.params.who}`)
+
+const notfound = (req, res) =>
+  send(res, 404, 'Not found route')
+
+module.exports = router(
+  get('/hello/:who', hello),
+  get('/*', notfound)
+)
